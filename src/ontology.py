9687e765c4fdd942ec48d85e0d3c944583bdbcc3
jmichuda <jack.michuda@tempus.com>
2022-03-02 22:46:07 -0800

    add cna and fusion

--- src/ontology.py
+++ src/ontology.py
@@ -1,5 +1,5 @@
 from owlready2 import *
-from src.generate_data import therapies, all_curated_genes,add_oncotree, parse_maf
+from src.generate_data import therapies, all_curated_genes,add_oncotree, parse_maf, add_fusions, add_cnas
 import re
 import types 
 import defopt
@@ -12,15 +12,14 @@ import os
 
 def main(maf_path: str, fusion_path: str, cna_path: str, output_path: str):
 	onto = get_ontology("ontology/base.owl").load()
-
 	with onto:
 		onto = therapies(onto)
 		onto = all_curated_genes(onto)
 		onto = add_oncotree(onto)
 		onto = add_oncotree(onto)
 		onto = parse_maf(onto, maf_path)
-
-		# save ontology to disk
+		onto = add_fusions(onto,fusion_path)
+		onto = add_cnas(onto, cna_path)
 		onto.save(file = output_path)
 
 if __name__ == "__main__":
